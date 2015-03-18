ac896c25adb30e2b2f0765373a33ac9801669867
Airse <erin_teo@msn.com>
2015-03-19 01:23:14 +0800

    added migration that adds status_last_updated_on column to confessions table

---
+++ sites/nuswhispers/database/migrations/2015_03_18_170822_add_status_last_updated_to_confessions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStatusLastUpdatedToConfessionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('confessions', function(Blueprint $table)
+		{
+			$table->timestamp('status_last_updated_on')->default(DB::raw('CURRENT_TIMESTAMP'));
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('confessions', function(Blueprint $table)
+		{
+			$table->dropColumn('status_last_updated_on');
+		});
+	}
+
+}
