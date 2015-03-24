756c80ef18ae50b9425133595f63d6863f2e87c5
Melvin Lee <zy@zy.sg>
2015-03-24 11:08:01 +0800

    Added migrations to revamp users table and add password resets table (Laravel default).

---
+++ sites/nuswhispers/database/migrations/2015_03_24_024252_revamp_users_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RevampUsersTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->dropColumn(['username', 'password']);
+        });
+
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->dropColumn(['name', 'email', 'password', 'created_at', 'updated_at', 'remember_token']);
+        });
+
+        Schema::create('users', function(Blueprint $table) {
+            $table->string('username', 255)->unique();
+            $table->string('password', 255);
+            $table->enum('role', array('Moderator', 'Administrator'))->default('Moderator');
+        });
+    }
+
+}
