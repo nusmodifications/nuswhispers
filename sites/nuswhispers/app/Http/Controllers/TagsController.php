9da032b94abf8a1538cb7616c65b8a0f185b3855
zhouyichen <zhouyichen123@gmail.com>
2015-04-03 16:20:39 +0800

    Fix coding style

--- sites/nuswhispers/app/Http/Controllers/TagsController.php
+++ sites/nuswhispers/app/Http/Controllers/TagsController.php
@@ -12,7 +12,8 @@ class TagsController extends Controller {
 	 * Get all tags in sorted order according to number of posts a tag belongs to.
 	 * @return array [tag1, tag2, ...]
 	 */
-	private function getSortedTags(){
+	private function getSortedTags()
+	{
 		$tags = Tag::get()->sortBy(function($tag)
 		{
 		    return -$tag->confessions()->count();
