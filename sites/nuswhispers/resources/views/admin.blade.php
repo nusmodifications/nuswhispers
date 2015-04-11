054d415e6f9284a9fe1f249bfa72f826605daf1e
Melvin Lee <zy@zy.sg>
2015-04-11 23:21:56 +0800

    Hid users management link for moderators.

--- sites/nuswhispers/resources/views/admin.blade.php
+++ sites/nuswhispers/resources/views/admin.blade.php
@@ -38,7 +38,9 @@
       </ul>
       <hr>
       <ul class="nav main-nav">
+        @if (\Auth::user()->role == 'Administrator')
         <li class="{{ Request::is('admin/users') ? 'active' : '' }}"><a href="/admin/users"><span class="typcn typcn-group"></span>User Management</a></li>
+        @endif
         <li><a href="/auth/logout"><span class="typcn typcn-eject"></span>Logout</a></li>
       </ul>
     </nav><!-- #sidebar-wrapper -->
