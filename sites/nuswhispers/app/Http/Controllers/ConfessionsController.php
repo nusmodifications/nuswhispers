e42a2460c00bc3692a4149260bdfd836c35733bb
Melvin Lee <zy@zy.sg>
2015-12-26 22:08:09 +0800

    Format to PSR-2

--- sites/nuswhispers/app/Http/Controllers/ConfessionsController.php
+++ sites/nuswhispers/app/Http/Controllers/ConfessionsController.php
@@ -1,17 +1,14 @@
 <?php namespace App\Http\Controllers;
 
-use App\Models\Tag as Tag;
-use App\Models\Confession as Confession;
-use App\Models\FbUser as FbUser;
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Confession;
+use App\Models\Tag;
 use App\Repositories\ConfessionsRepository;
 
-use Illuminate\Http\Request;
-
-class ConfessionsController extends Controller {
-
+class ConfessionsController extends Controller
+{
     const MAX_CONFESSION_COUNT = 10;
+
     const MAX_TAG_COUNT = 30;
 
     protected $confessionsRepo;
@@ -19,6 +16,68 @@ class ConfessionsController extends Controller {
     public function __construct(ConfessionsRepository $confessionsRepo)
     {
         $this->confessionsRepo = $confessionsRepo;
+    }
+
+    public function category($categoryId)
+    {
+        $query = Confession::orderBy('status_updated_at', 'DESC')
+            ->join('confession_categories', 'confessions.confession_id', '=', 'confession_categories.confession_id')
+            ->where('confession_categories.confession_category_id', '=', $categoryId)
+            ->approved()
+            ->with('favourites')
+            ->with('categories');
+
+        if (\Input::get('timestamp')) {
+            $query->whereRaw('UNIX_TIMESTAMP(status_updated_at) <= ?', [\Input::get('timestamp')]);
+        }
+        if (\Input::get('count') > 0) {
+            $query->take(\Input::get('count'));
+            $query->skip(\Input::get('offset'));
+        }
+
+        $confessions = $query->get();
+        foreach ($confessions as $confession) {
+            $confession->status_updated_at_timestamp = $confession->status_updated_at->timestamp;
+            $confession->getFacebookInformation();
+        }
+        return \Response::json(['data' => ['confessions' => $confessions]]);
+    }
+
+    public function favourites()
+    {
+        $fbUserId = \Session::get('fb_user_id');
+        if ($fbUserId) {
+            $query = Confession::join('favourites', 'confessions.confession_id', '=', 'favourites.confession_id')
+                ->where('favourites.fb_user_id', '=', $fbUserId)
+                ->orderBy('status_updated_at', 'DESC')
+                ->approved()
+                ->with('favourites')
+                ->with('categories');
+
+            if (intval(\Input::get('count')) == 0) {
+                $count = self::MAX_CONFESSION_COUNT;
+            } else {
+                $count = min(intval(\Input::get('count')), self::MAX_CONFESSION_COUNT);
+            }
+
+            if (\Input::get('timestamp')) {
+                $query->whereRaw('UNIX_TIMESTAMP(status_updated_at) <= ?', [\Input::get('timestamp')]);
+            }
+
+            $query->take($count);
+
+            if (intval(\Input::get('offset')) > 0) {
+                $query->skip(intval(\Input::get('offset')));
+            }
+
+            $confessions = $query->get();
+            foreach ($confessions as $confession) {
+                $confession->status_updated_at_timestamp = $confession->status_updated_at->timestamp;
+                $confession->getFacebookInformation();
+            }
+            return \Response::json(['success' => true, 'data' => ['confessions' => $confessions]]);
+        }
+        return \Response::json(['success' => false, 'errors' => ['User not logged in.']]);
     }
 
     /**
@@ -33,41 +92,11 @@ class ConfessionsController extends Controller {
             ->featured()
             ->orderBy('status_updated_at', 'DESC');
 
-        if (intval(\Input::get('count')) == 0)
+        if (intval(\Input::get('count')) == 0) {
             $count = self::MAX_CONFESSION_COUNT;
-        else
+        } else {
             $count = min(intval(\Input::get('count')), self::MAX_CONFESSION_COUNT);
-
-        if (\Input::get('timestamp')) {
-            $query->whereRaw('UNIX_TIMESTAMP(status_updated_at) <= ?', [\Input::get('timestamp')]);
         }
-
-        $query->take($count);
-
-        if (intval(\Input::get('offset')) > 0) {
-            $query->skip(intval(\Input::get('offset')));
-        }
-
-        $confessions = $query->get();
-        foreach ($confessions as $confession) {
-            $confession->status_updated_at_timestamp = $confession->status_updated_at->timestamp;
-            $confession->getFacebookInformation();
-        }
-
-        return \Response::json(['data' => ['confessions' => $confessions]]);
-    }
-
-    public function recent()
-    {
-        $query = Confession::with('categories')
-            ->with('favourites')
-            ->approved()
-            ->orderBy('status_updated_at', 'DESC');
-
-        if (intval(\Input::get('count')) == 0)
-            $count = self::MAX_CONFESSION_COUNT;
-        else
-            $count = min(intval(\Input::get('count')), self::MAX_CONFESSION_COUNT);
 
         if (\Input::get('timestamp')) {
             $query->whereRaw('UNIX_TIMESTAMP(status_updated_at) <= ?', [\Input::get('timestamp')]);
@@ -98,10 +127,11 @@ class ConfessionsController extends Controller {
             ->with('favourites')
             ->with('categories');
 
-        if (intval(\Input::get('count')) == 0)
+        if (intval(\Input::get('count')) == 0) {
             $count = self::MAX_CONFESSION_COUNT;
-        else
+        } else {
             $count = min(intval(\Input::get('count')), self::MAX_CONFESSION_COUNT);
+        }
 
         if (\Input::get('timestamp')) {
             $query->whereRaw('UNIX_TIMESTAMP(status_updated_at) <= ?', [\Input::get('timestamp')]);
@@ -121,50 +151,60 @@ class ConfessionsController extends Controller {
         return \Response::json(['data' => ['confessions' => $confessions]]);
     }
 
-    public function category($categoryId)
+    public function recent()
     {
+        $query = Confession::with('categories')
+            ->with('favourites')
+            ->approved()
+            ->orderBy('status_updated_at', 'DESC');
+
+        if (intval(\Input::get('count')) == 0) {
+            $count = self::MAX_CONFESSION_COUNT;
+        } else {
+            $count = min(intval(\Input::get('count')), self::MAX_CONFESSION_COUNT);
+        }
+
+        if (\Input::get('timestamp')) {
+            $query->whereRaw('UNIX_TIMESTAMP(status_updated_at) <= ?', [\Input::get('timestamp')]);
+        }
+
+        $query->take($count);
+
+        if (intval(\Input::get('offset')) > 0) {
+            $query->skip(intval(\Input::get('offset')));
+        }
+
+        $confessions = $query->get();
+        foreach ($confessions as $confession) {
+            $confession->status_updated_at_timestamp = $confession->status_updated_at->timestamp;
+            $confession->getFacebookInformation();
+        }
+
+        return \Response::json(['data' => ['confessions' => $confessions]]);
+    }
+
+    /**
+     * Search for confessions which contains the search string
+     * method: get
+     * route: api/confessions/search/<searchString>?timestamp=<time>&offset=<offset>&count=<count>
+     * @param  string $searchString - non-empty string (of length at least 5? - maybe at least 1)
+     * @return json {"data": {"confessions": [Confession1, confession2, ...]}}
+     *                           an array of confession json
+     */
+    public function search($searchString)
+    {
+        // Naive search ...
         $query = Confession::orderBy('status_updated_at', 'DESC')
-            ->join('confession_categories', 'confessions.confession_id' , '=', 'confession_categories.confession_id')
-            ->where('confession_categories.confession_category_id', '=', $categoryId)
+            ->where('content', 'LIKE', '%' . $searchString . '%')
             ->approved()
             ->with('favourites')
             ->with('categories');
 
-        if (\Input::get('timestamp')) {
-            $query->whereRaw('UNIX_TIMESTAMP(status_updated_at) <= ?', [\Input::get('timestamp')]);
+        if (intval(\Input::get('count')) == 0) {
+            $count = self::MAX_CONFESSION_COUNT;
+        } else {
+            $count = min(intval(\Input::get('count')), self::MAX_CONFESSION_COUNT);
         }
-        if (\Input::get('count') > 0) {
-            $query->take(\Input::get('count'));
-            $query->skip(\Input::get('offset'));
-        }
-
-        $confessions = $query->get();
-        foreach ($confessions as $confession) {
-            $confession->status_updated_at_timestamp = $confession->status_updated_at->timestamp;
-            $confession->getFacebookInformation();
-        }
-        return \Response::json(['data' => ['confessions' => $confessions]]);
-    }
-
-    public function tag($tagName)
-    {
-        $query = Confession::select('confessions.*')
-            ->leftJoin('confession_tags', 'confessions.confession_id' , '=', 'confession_tags.confession_id')
-            ->leftJoin('tags', 'confession_tags.confession_tag_id' , '=', 'tags.confession_tag_id')
-            ->where(function ($query) use ($tagName)
-            {
-                $query->where('tags.confession_tag', '=', "#$tagName")
-                    ->orWhere('confessions.confession_id', '=', $tagName);
-            })
-            ->orderBy('status_updated_at', 'DESC')
-            ->approved()
-            ->with('favourites')
-            ->with('categories');
-
-        if (intval(\Input::get('count')) == 0)
-            $count = self::MAX_TAG_COUNT;
-        else
-            $count = min(intval(\Input::get('count')), self::MAX_TAG_COUNT);
 
         if (\Input::get('timestamp')) {
             $query->whereRaw('UNIX_TIMESTAMP(status_updated_at) <= ?', [\Input::get('timestamp')]);
@@ -176,8 +216,7 @@ class ConfessionsController extends Controller {
             $query->skip(intval(\Input::get('offset')));
         }
 
-        $confessions = $query->distinct()->get();
-
+        $confessions = $query->get();
         foreach ($confessions as $confession) {
             $confession->status_updated_at_timestamp = $confession->status_updated_at->timestamp;
             $confession->getFacebookInformation();
@@ -185,40 +224,25 @@ class ConfessionsController extends Controller {
         return \Response::json(['data' => ['confessions' => $confessions]]);
     }
 
-    public function favourites()
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
     {
-        $fbUserId = \Session::get('fb_user_id');
-        if ($fbUserId) {
-            $query = Confession::join('favourites', 'confessions.confession_id' , '=', 'favourites.confession_id')
-            ->where('favourites.fb_user_id', '=', $fbUserId)
-            ->orderBy('status_updated_at', 'DESC')
-            ->approved()
-            ->with('favourites')
-            ->with('categories');
+        $confession = Confession::with('categories')->with('favourites')->find($id);
+        if ($confession && $confession->isApproved()) {
+            // increment number of views
+            $confession->views++;
+            $confession->save();
 
-            if (intval(\Input::get('count')) == 0)
-                $count = self::MAX_CONFESSION_COUNT;
-            else
-                $count = min(intval(\Input::get('count')), self::MAX_CONFESSION_COUNT);
-
-            if (\Input::get('timestamp')) {
-                $query->whereRaw('UNIX_TIMESTAMP(status_updated_at) <= ?', [\Input::get('timestamp')]);
-            }
-
-            $query->take($count);
-
-            if (intval(\Input::get('offset')) > 0) {
-                $query->skip(intval(\Input::get('offset')));
-            }
-
-            $confessions = $query->get();
-            foreach ($confessions as $confession) {
-                $confession->status_updated_at_timestamp = $confession->status_updated_at->timestamp;
-                $confession->getFacebookInformation();
-            }
-            return \Response::json(['success' => true, 'data' => ['confessions' => $confessions]]);
+            $confession->status_updated_at_timestamp = $confession->status_updated_at->timestamp;
+            $confession->getFacebookInformation();
+            return \Response::json(['success' => true, 'data' => ['confession' => $confession]]);
         }
-        return \Response::json(['success' => false, 'errors' =>['User not logged in.']]);
+        return \Response::json(['success' => false]);
     }
 
     /**
@@ -252,61 +276,37 @@ class ConfessionsController extends Controller {
         if (is_array(\Input::get('categories'))) {
             $res = $this->confessionsRepo->create([
                 'content' => \Input::get('content'),
-                'images'  => \Input::get('image'),
+                'images' => \Input::get('image')
             ], \Input::get('categories'));
         } else {
             $res = $this->confessionsRepo->create([
                 'content' => \Input::get('content'),
-                'images'  => \Input::get('image'),
+                'images' => \Input::get('image')
             ]);
         }
-
 
         return \Response::json(['success' => $res]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
+    public function tag($tagName)
     {
-        $confession = Confession::with('categories')->with('favourites')->find($id);
-        if ($confession && $confession->isApproved())  {
-            // increment number of views
-            $confession->views++;
-            $confession->save();
-
-            $confession->status_updated_at_timestamp = $confession->status_updated_at->timestamp;
-            $confession->getFacebookInformation();
-            return \Response::json(['success' => true, 'data' => ['confession' => $confession]]);
-        }
-        return \Response::json(['success' => false]);
-    }
-
-    /**
-     * Search for confessions which contains the search string
-     * method: get
-     * route: api/confessions/search/<searchString>?timestamp=<time>&offset=<offset>&count=<count>
-     * @param  string $searchString - non-empty string (of length at least 5? - maybe at least 1)
-     * @return json {"data": {"confessions": [Confession1, confession2, ...]}}
-     *                           an array of confession json
-     */
-    public function search($searchString)
-    {
-        // Naive search ...
-        $query = Confession::orderBy('status_updated_at', 'DESC')
-            ->where('content', 'LIKE', '%'.$searchString.'%')
+        $query = Confession::select('confessions.*')
+            ->leftJoin('confession_tags', 'confessions.confession_id', '=', 'confession_tags.confession_id')
+            ->leftJoin('tags', 'confession_tags.confession_tag_id', '=', 'tags.confession_tag_id')
+            ->where(function ($query) use ($tagName) {
+                $query->where('tags.confession_tag', '=', "#$tagName")
+                    ->orWhere('confessions.confession_id', '=', $tagName);
+            })
+            ->orderBy('status_updated_at', 'DESC')
             ->approved()
             ->with('favourites')
             ->with('categories');
 
-        if (intval(\Input::get('count')) == 0)
-            $count = self::MAX_CONFESSION_COUNT;
-        else
-            $count = min(intval(\Input::get('count')), self::MAX_CONFESSION_COUNT);
+        if (intval(\Input::get('count')) == 0) {
+            $count = self::MAX_TAG_COUNT;
+        } else {
+            $count = min(intval(\Input::get('count')), self::MAX_TAG_COUNT);
+        }
 
         if (\Input::get('timestamp')) {
             $query->whereRaw('UNIX_TIMESTAMP(status_updated_at) <= ?', [\Input::get('timestamp')]);
@@ -318,7 +318,8 @@ class ConfessionsController extends Controller {
             $query->skip(intval(\Input::get('offset')));
         }
 
-        $confessions = $query->get();
+        $confessions = $query->distinct()->get();
+
         foreach ($confessions as $confession) {
             $confession->status_updated_at_timestamp = $confession->status_updated_at->timestamp;
             $confession->getFacebookInformation();
