396e3432f159b2dbd6cb821bdb6c2aa6ef2156ba
Melvin Lee <zy@zy.sg>
2017-01-01 03:54:05 +0800

    Do not serialize model on ConfessionWasDeleted event

--- app/Events/ConfessionWasDeleted.php
+++ app/Events/ConfessionWasDeleted.php
@@ -2,6 +2,26 @@
 
 namespace NUSWhispers\Events;
 
-class ConfessionWasDeleted extends BaseConfessionEvent
+use NUSWhispers\Models\User;
+use NUSWhispers\Models\Confession;
+
+class ConfessionWasDeleted
 {
+    /** @var \NUSWhispers\Models\Confession */
+    public $confession;
+
+    /** @var \NUSWhispers\Models\User */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param \NUSWhispers\Models\Confession $confession
+     * @param \NUSWhispers\Models\User|null  $user
+     */
+    public function __construct(Confession $confession, User $user = null)
+    {
+        $this->confession = $confession;
+        $this->user = $user;
+    }
 }
