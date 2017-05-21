8a2095fd08e92e314ccd205fffaa07a892a80dfb
Melvin Lee <zy@zy.sg>
2017-05-21 20:20:58 +0800

    Add more headers required for CORS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,8 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization')
+            ->header('X-Content-Type-Options', 'nosniff');
     }
 }
