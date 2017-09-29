e9db24a83c66e4fca3c951597cc1887fa18986ca
Melvin Lee <zy@zy.sg>
2017-09-29 23:05:59 +0800

    Setup admin Blade templates to use assets generated by Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,1 +1,13 @@
 const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application.
+ |
+ */
+mix.js('resources/assets/js/admin.js', 'public/js')
+    .sass('resources/assets/sass/admin.scss', 'public/css');
