159836e4d5436c31b40cd875106a6ec1851ee166
Melvin Lee <zy@zy.sg>
2018-03-04 17:40:20 +0800

    Use UUID for less conflicts on Buddy CI

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,7 +1,9 @@
 <?php
 
+use Ramsey\Uuid\Uuid;
+
 $factory->define(\NUSWhispers\Models\Category::class, function ($faker) {
     return [
-        'confession_category' => ucfirst($faker->word) . microtime(true),
+        'confession_category' => ucfirst($faker->word) . '-' . (string) Uuid::uuid4(),
     ];
 });
