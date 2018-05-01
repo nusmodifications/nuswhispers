68ae375fa89771628cdfd85df7168df09afc058a
Melvin Lee <zy@zy.sg>
2018-05-02 00:27:25 +0800

    Remove more instances of using Typekit

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -5,9 +5,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>NUSWhispers</title>
-
-    <script src="//use.typekit.net/zog5enw.js"></script>
-    <script>try{Typekit.load({ async: true });}catch(e){}</script>
 	<link rel="stylesheet" href="/assets/css/admin.css">
 </head>
 <body>
