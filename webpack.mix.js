a1b60a249750959d9eb43747c61122f381988f03
Melvin Lee <zy@zy.sg>
2018-10-13 22:45:50 +0800

    Add & setup new admin dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,3 +10,34 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+
+mix
+  .autoload({
+    jquery: ['$', 'window.jQuery'],
+  })
+  .options({
+    uglify: {
+      extractComments: true,
+    },
+  })
+  .webpackConfig({
+    resolve: {
+      alias: {
+        jquery: 'jquery/dist/jquery.slim.js',
+      },
+    },
+  })
+  .js('resources/js/admin.js', 'public/js')
+  .extract(['bootstrap', 'jquery', 'flatpickr'])
+  .sass('resources/sass/admin.scss', 'public/css')
+  .styles(
+    [
+      'node_modules/typicons.font/src/font/typicons.css',
+      'public/css/admin.css',
+    ],
+    'public/css/admin.css'
+  );
+
+if (mix.inProduction()) {
+  mix.sourceMaps().version();
+}
