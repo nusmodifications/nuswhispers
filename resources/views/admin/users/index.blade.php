332a8a27a14f2f7baeeae586eb371c7a44efde7d
Melvin Lee <zy@zy.sg>
2018-10-17 17:45:55 +0800

    Rework Manage Users views

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,52 +1,51 @@
-@extends('admin')
+@extends('layouts.admin')
+
+@section('title', 'Manage Users')
 
 @section('content')
-<form id="users-search-form" class="search-form form-inline" method="get">
 
-  <div class="page-header">
-    <h1 class="page-title"><span class="typcn typcn-group"></span>Users Management <a href="/admin/users/add" class="btn btn-sm btn-primary">Add New User</a></h1>
+<div class="page-header">
+    <h1>
+        <span class="typcn typcn-group"></span>
+        Users Management
+    </h1>
+    <div>
+        <a href="/admin/users/add" class="btn btn-sm btn-primary">Add New User</a>
+    </div>
+</div>
 
-  </div>
-
-  @include('message')
-
-  <div class="search-filters">
-    <?php echo str_replace('pagination', 'pagination pagination-sm', $users->render()); ?>
-  </div>
-
-  <table id="user-list" class="user-list table table-striped table-hover">
+<table class="table table-bordered table-striped table-hover">
     <thead>
-      <tr>
-        <th>Email Address</th>
-        <th>Display Name</th>
-        <th>Role</th>
-        <th>Action(s)</th>
-      </tr>
+        <tr>
+            <th>Email Address</th>
+            <th>Display Name</th>
+            <th>Role</th>
+            <th>Action(s)</th>
+        </tr>
     </thead>
     <tbody>
-    @foreach ($users as $user)
-    <tr>
-      <td>{{$user->email}}</td>
-      <td>{{$user->name ?: '-'}}</td>
-      <td>{{$user->role}}</td>
-      <td>
-        <a class="btn btn-sm" href="/admin/users/edit/{{ $user->user_id }}">
-          Edit
-        </a>
-        @if (auth()->user()->user_id !== $user->user_id)
-        <a class="btn btn-sm btn-danger" href="/admin/users/delete/{{ $user->user_id }}">
-          Delete
-        </a>
-        @endif
-      </td>
-    </tr>
-    @endforeach
+        @foreach ($users as $user)
+        <tr>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->name ?: '-' }}</td>
+            <td>{{ $user->role }}</td>
+            <td>
+                <a class="btn btn-secondary btn-sm" href="/admin/users/edit/{{ $user->user_id }}">
+                    Edit
+                </a>
+                @if (auth()->user()->user_id !== $user->user_id)
+                <a class="btn btn-sm btn-danger" href="/admin/users/delete/{{ $user->user_id }}">
+                    Delete
+                </a>
+                @endif
+            </td>
+        </tr>
+        @endforeach
     </tbody>
-  </table>
+</table>
 
-  <div class="search-filters">
-    <?php echo str_replace('pagination', 'pagination pagination-sm', $users->render()); ?>
-  </div>
+<div class="d-flex my-4 justify-content-end">
+    {{ $users->links('admin.pagination') }}
+</div>
 
-</form><!-- #users-search-form -->
 @endsection
