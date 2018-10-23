57e6fd5e317dfeb739a2fc19c309608fb7086a47
Melvin Lee <zy@zy.sg>
2018-10-23 20:02:13 +0800

    Simplify babel config

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,20 +1,3 @@
-module.exports = api => {
-  const config = { presets: [] };
-
-  config.presets.push(
-    api.env('test')
-      ? [
-          'next/babel',
-          {
-            'preset-env': {
-              modules: 'commonjs',
-            },
-          },
-        ]
-      : 'next/babel',
-  );
-
-  config.presets.push('@zeit/next-typescript/babel');
-
-  return config;
+module.exports = {
+  presets: ['next/babel', '@zeit/next-typescript/babel'],
 };
