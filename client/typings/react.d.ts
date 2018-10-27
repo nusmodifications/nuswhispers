dafd3b95538ba551964041fd8ad2e0c5d608097c
Melvin Lee <zy@zy.sg>
2018-10-27 23:04:25 +0800

    Test React 16.7.0-alpha.0

---
+++ client/typings/react.d.ts
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+declare module 'react' {
+  // @TODO: Remove when the TypeScript definitions for React 16.7 is available.
+  function useState<T>(initialState: T | (() => T)): [T, (newState: T) => void];
+  function useEffect(
+    create: () => void | (() => void),
+    inputs?: ReadonlyArray<unknown>,
+  ): void;
+  function useContext<T>(foo: React.Context<T>): T;
+  function useReducer<S, A>(
+    reducer: (state: S, action: A) => S,
+    initialState: S,
+  ): [S, (action: A) => void];
+  function useCallback<F extends (...args: never[]) => unknown>(
+    callback: F,
+    inputs?: ReadonlyArray<unknown>,
+  ): F;
+  function useMemo<T>(create: () => T, inputs?: ReadonlyArray<unknown>): T;
+  function useRef<T extends unknown>(initialValue?: T): React.RefObject<T>;
+  function useImperativeMethods<T>(
+    ref: React.Ref<T>,
+    createInstance: () => T,
+    inputs?: ReadonlyArray<unknown>,
+  ): void;
+  const useMutationEffect: typeof useEffect;
+  const useLayoutEffect: typeof useEffect;
+}
