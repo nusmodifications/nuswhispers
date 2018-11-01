e6dfaa4b00727a0b88ba1ab1af0395d876ba80fc
Melvin Lee <zy@zy.sg>
2018-11-01 23:40:37 +0800

    Transpile node dependencies so they would work with browsers

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,23 @@
+const path = require('path');
 const withTypescript = require('@zeit/next-typescript');
 
-module.exports = withTypescript();
+const resolveModule = dir => path.join(__dirname, '../node_modules', dir);
+
+module.exports = withTypescript({
+  webpack: (config, { defaultLoaders }) => {
+    // Transpile Node dependencies.
+    config.module.rules.push({
+      test: /\.js$/,
+      loader: defaultLoaders.babel,
+      exclude: /node_modules/,
+      include: [
+        resolveModule('camelcase'),
+        resolveModule('camelcase-keys'),
+        resolveModule('map-obj'),
+        resolveModule('quick-lru'),
+      ],
+    });
+
+    return config;
+  },
+});
