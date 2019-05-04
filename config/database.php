2fd3a12e58d55356ac3ed6adce44529f1834d4e1
Melvin Lee <zy@zy.sg>
2019-05-04 22:34:56 +0800

    Check for pdo_mysql extension in config to prevent PHP errors

--- config/database.php
+++ config/database.php
@@ -64,9 +64,9 @@ return [
             'prefix_indexes' => true,
             'strict' => false,
             'engine' => null,
-            'options' => array_filter([
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]),
+            ]) : [],
         ],
 
         'pgsql' => [
@@ -103,9 +103,9 @@ return [
             'prefix_indexes' => true,
             'strict' => false,
             'engine' => null,
-            'options' => array_filter([
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]),
+            ]) : [],
         ],
 
     ],
